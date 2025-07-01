d39c99cf12271553d3c54c5fc6aaae66fe3d2713
Vishesh Dwivedi <visheshd223@gmail.com>
2025-07-01 14:07:41 +0530

    updated css in sign-up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -145,7 +145,7 @@ export default function SignUpForm() {
                 control={form.control}
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-white">Username or Email</FormLabel>
+                    <FormLabel className="text-white">Email</FormLabel>
                     <FormControl>
                       <div className="relative">
                         <Mail className="absolute left-3 top-3 h-5 w-5 text-cyan-400" />
